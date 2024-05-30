84c1c98a787158b50c32d087f1eaa479cef2c4a3
Code Mode <codemodeuser@gmail.com>
2024-05-30 16:38:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import { tabs } from "./Data/tabs.jsx";
 
 function App() {
+  let [todolist, setTodolist] = useState([]);
+  let [activeTabs, setactiveTabs] = useState(0);
+  let [activeContent, setActiveContent] = useState(tabs[0]);
+  
+
+
+
+  let changeData=(index)=>{
+    setactiveTabs(index)
+    setActiveContent(tabs[index])
+  }
+  let saveToDoList = (event) => {
+    let toname = event.target.toname.value;
+    if (!todolist.includes(toname)) {
+      let finalDolist = [...todolist, toname];
+      setTodolist(finalDolist);
+    } else {
+      alert("Todo name already exists!");
+    }
+
+    event.preventDefault();
+  };
+
+  let list = todolist.map((value, index) => {
+    return (
+      <ToDoListItems
+        value={value}
+        key={index}
+        indexNumber={index}
+        todolist={todolist}
+        setTodolist={setTodolist}
+      />
+    );
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div className="tabsOuter">
+        <h1 style={{ textAlign: "left" }}>
+          REACT PROJECT
+        </h1>
+        <ul>
+          {/* {console.log(tabs)} */}
+          {tabs.map((tabsItems, index) => {
+            return (
+              <li>
+                <button onClick={()=>changeData(index)} className={activeTabs===index?"activeButton":""} key={index}>{tabsItems.title}</button>
+              </li>
+            )
+          })}
+        </ul>
+        {activeContent !== undefined ? 
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {activeContent.description}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        :
+        ""}
+      </div>
+      <h1>Todo List</h1>
+      <form onSubmit={saveToDoList}>
+        <input type="text" name="toname" />
+        <button>Save</button>
+      </form>
+
+      <div className="outerDiv">
+        <ul>{list}</ul>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+function ToDoListItems({ value, indexNumber, todolist, setTodolist }) {
+  let [status, setStatus] = useState(false);
+  let deleteNow = () => {
+    let finalData = todolist.filter((v, i) => i !== indexNumber);
+    setTodolist(finalData);
+  };
+
+  return (
+    <li
+      className={status ? "completetodo" : ""}
+      onClick={() => setStatus(!status)}
+    >
+      {indexNumber + 1}. {value} <span onClick={deleteNow}>&times;</span>
+    </li>
+  );
+}
---
+++ src/Data/tabs.jsx
@@ -0,0 +1,17 @@
+export let tabs = [
+  {
+    "title": "Vision",
+    "description":
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae itaque reiciendis assumenda consequatur illum. Repudiandae eaque, distinctio nesciunt quibusdam officia cumque optio eveniet voluptatibus corrupti, nulla perferendis iste vel et",
+  },
+  {
+    "title": "Mission",
+    "description":
+      "orem ipsum dolor sit amet consectetur adipisicing elit. Beatae itaque reiciendis assumenda consequatur illum. Repudiandae eaque, distinctio nesciunt quibusdam officia cumque optio eveniet voluptatibus corrupti, nulla perferendis iste vel et",
+  },
+  {
+    "title": "Values",
+    "description":
+      "rem ipsum dolor sit amet consectetur adipisicing elit. Beatae itaque reiciendis assumenda consequatur illum. Repudiandae eaque, distinctio nesciunt quibusdam officia cumque optio eveniet voluptatibus corrupti, nulla perferendis iste vel et",
+  },
+];
